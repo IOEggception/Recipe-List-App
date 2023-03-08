7bcd6d2c4e968a27d65e839ed29ff8ccf24e6af3
Sean Lacey <sean.lacey@libertymutual.com>
2023-03-08 11:37:37 -0500

    Add tabview as the starting point for the app

---
+++ RecipeListApp/View/RecipeTabView.swift
@@ -0,0 +1,39 @@
+//
+//  RecipeTabView.swift
+//  RecipeListApp
+//
+//  Created by Sean.Lacey on 3/8/23.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    
+    var body: some View {
+        
+        TabView {
+            Text( "Features view")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
+                }
+            
+            RecipeListView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "list.bullet")
+                        Text("Recipe List View")
+                    }
+                }
+        }
+    }
+    
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
